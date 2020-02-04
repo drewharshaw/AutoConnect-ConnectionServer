918de070ffc0c4a82ba827880bd1eac528993f9f
Heterdaft <39243852+heterdaft@users.noreply.github.com>
2020-02-03 21:51:06 -0500

    Increase the number of vehicle colors in visualization

--- src/public/js/CarSimulator.js
+++ src/public/js/CarSimulator.js
@@ -16,31 +16,120 @@ var tileUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 map.addLayer(layer);
 
 // Load in all custom icons with color information
-var redIcons = {
-  color: 'red',
-  standard: L.divIcon({ id: 'redIcon', className: 'red-icon', iconSize: null }),
-  communicating: L.divIcon({
-    id: 'redIconConnecting',
-    className: 'red-icon-connecting',
-    iconSize: null,
-  }),
-};
-
-var blueIcons = {
-  color: 'blue',
-  standard: L.divIcon({
-    id: 'blueIcon',
-    className: 'blue-icon',
-    iconSize: null,
-  }),
-  communicating: L.divIcon({
-    id: 'blueIconConnecting',
-    className: 'blue-icon-connecting',
-    iconSize: null,
-  }),
-};
 // Create an array to hold all the icon and color information
-var icons = [redIcons, blueIcons];
+var icons = [
+    {
+      color: 'red',
+      standard: L.divIcon({ id: 'redIcon', className: 'red-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'redIconConnecting', className: 'red-icon-connecting', iconSize: null }),
+    },
+    {
+      color: 'blue',
+      standard: L.divIcon({ id: 'blueIcon', className: 'blue-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'blueIconConnecting', className: 'blue-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'lightsalmon',
+      standard: L.divIcon({ id: 'lightsalmonIcon', className: 'lightsalmon-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'lightsalmonIconConnecting', className: 'lightsalmon-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'firebrick',
+      standard: L.divIcon({ id: 'firebrickIcon', className: 'firebrick-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'firebrickIconConnecting', className: 'firebrick-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'tomato',
+      standard: L.divIcon({ id: 'tomatoIcon', className: 'tomato-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'tomatoIconConnecting', className: 'tomato-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'lawngreen',
+      standard: L.divIcon({ id: 'lawngreenIcon', className: 'lawngreen-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'lawngreenIconConnecting', className: 'lawngreen-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'lime',
+      standard: L.divIcon({ id: 'limeIcon', className: 'lime-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'limeIconConnecting', className: 'lime-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'darkcyan',
+      standard: L.divIcon({ id: 'darkcyanIcon', className: 'darkcyan-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'darkcyanIconConnecting', className: 'darkcyan-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'midnightblue',
+      standard: L.divIcon({ id: 'midnightblueIcon', className: 'midnightblue-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'midnightblueIconConnecting', className: 'midnightblue-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'deeppink',
+      standard: L.divIcon({ id: 'deeppinkIcon', className: 'deeppink-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'deeppinkIconConnecting', className: 'deeppink-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'maroon',
+      standard: L.divIcon({ id: 'maroonIcon', className: 'maroon-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'maroonIconConnecting', className: 'maroon-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'saddlebrown',
+      standard: L.divIcon({ id: 'saddlebrownIcon', className: 'saddlebrown-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'saddlebrownIconConnecting', className: 'saddlebrown-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'gray',
+      standard: L.divIcon({ id: 'grayIcon', className: 'gray-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'grayIconConnecting', className: 'gray-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'darkslategray',
+      standard: L.divIcon({ id: 'darkslategrayIcon', className: 'darkslategray-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'darkslategrayIconConnecting', className: 'darkslategray-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'indigo',
+      standard: L.divIcon({ id: 'indigoIcon', className: 'indigo-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'indigoIconConnecting', className: 'indigo-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'magenta',
+      standard: L.divIcon({ id: 'magentaIcon', className: 'magenta-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'magentaIconConnecting', className: 'magenta-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'mediumslateblue',
+      standard: L.divIcon({ id: 'mediumslateblueIcon', className: 'mediumslateblue-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'mediumslateblueIconConnecting', className: 'mediumslateblue-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'cyan',
+      standard: L.divIcon({ id: 'cyanIcon', className: 'cyan-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'cyanIconConnecting', className: 'cyan-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'olive',
+      standard: L.divIcon({ id: 'oliveIcon', className: 'olive-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'oliveIconConnecting', className: 'olive-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'mediumseagreen',
+      standard: L.divIcon({ id: 'mediumseagreenIcon', className: 'mediumseagreen-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'mediumseagreenIconConnecting', className: 'mediumseagreen-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'green',
+      standard: L.divIcon({ id: 'greenIcon', className: 'green-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'greenIconConnecting', className: 'green-icon-connecting',iconSize: null }),
+    },
+    {
+      color: 'yellow',
+      standard: L.divIcon({ id: 'yellowIcon', className: 'yellow-icon', iconSize: null }),
+      communicating: L.divIcon({ id: 'yellowIconConnecting', className: 'yellow-icon-connecting', iconSize: null }),
+    }
+    
+];
 
 // Create global markers map. This will be populated by info in the trajectory file
 // The map is keyed by "Vehicle ID"
